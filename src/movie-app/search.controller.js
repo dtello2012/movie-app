f7e223be1b1dabb9fa9469372889fdc37f5b7529
David Tello <dtello@seamgen.com>
2016-03-15 10:46:46 -0700

    search controller and test

---
+++ src/movie-app/search.controller.js
@@ -0,0 +1,14 @@
+
+(function() {
+	"use strict";
+	angular.module('movieApp', [])
+			.controller('SearchController', ['$scope', '$location', SearchController]);
+	function SearchController ($scope, $location) {
+		$scope.search = function() {
+			if($scope.query) {
+				$location.path('/results').search('q', $scope.query);
+			}
+		};
+	}
+
+}());
